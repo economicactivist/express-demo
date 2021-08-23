31b46f2b31b7c60576e3838486ba2e83d5fd52a7
Mountain Scott <mountainscott@yahoo.com>
2021-08-23 09:14:30 -0700

    added validation to post endpoint

---
+++ index.js
@@ -0,0 +1,69 @@
+const express = require('express')
+const Joi = require('joi')
+const app = express()
+const port = process.env.PORT || 3000
+
+
+app.use(express.json()) 
+
+// name_schema = Joi.object().keys({
+//     name: Joi.string().required()
+// })
+
+name_schema = Joi.object().keys({
+    name: Joi.string().min(3).required()
+})
+
+
+const validate = (schema, data) => {
+
+    const result = schema.validate(data)
+    if (result.error) {
+        return result.error.details[0].message  // return the first error message
+    }
+    return null
+}
+
+
+const courses = [   // array of objects
+    { id: 1, name: 'course1' },
+    { id: 2, name: 'course2' },
+    { id: 3, name: 'course3' }
+]
+
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.get('/api/courses', (req, res) => {
+  res.send(courses)
+})
+
+app.get('/api/courses/:id', (req, res) => {
+  
+  const course = courses.find(c => c.id === parseInt(req.params.id))
+  if(!course) res.status(404).send('The course with the given ID was not found')
+  res.send(course)
+
+
+})
+
+app.post('/api/courses', (req, res) => {
+ 
+
+    const validationError = validate(name_schema, req.body)
+    if (validationError) {
+        res.status(400).send(validationError)
+        return
+    }  // if validation error, return 400 and send the error message
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name
+    }
+
+    courses.push(course)
+    res.send(course) //so the client can get the id of the new course
+})
+
+
+    // export PORT = 5000
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
